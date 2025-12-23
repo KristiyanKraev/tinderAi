f783c7a70808c049e1e6f2ccddd378131bbfb41b
kristiyan <bazzana87@gmail.com>
2025-12-24 00:12:02 +0200

    initial commit

---
+++ src/main/java/projects/koko/tinder_backend/TinderBackendApplication.java
@@ -0,0 +1,13 @@
+package projects.koko.tinder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderBackendApplication.class, args);
+	}
+
+}
