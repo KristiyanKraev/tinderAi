49652b2c52a076cb660a1818ad32210277de3bd2
kristiyan <bazzana87@gmail.com>
2026-01-15 23:58:15 +0200

    added getUserProfile for user profile page

--- backend/src/main/java/projects/koko/tinder_backend/profiles/ProfileController.java
+++ backend/src/main/java/projects/koko/tinder_backend/profiles/ProfileController.java
@@ -1,6 +1,5 @@
 package projects.koko.tinder_backend.profiles;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/profiles")
 public class ProfileController {
 
-    @Autowired
-    private ProfileRepository profileRepository;
+    private final ProfileRepository profileRepository;
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileRepository profileRepository, ProfileService profileService) {
+        this.profileRepository = profileRepository;
+        this.profileService = profileService;
+    }
+
 
     @CrossOrigin(origins = "*")
     @GetMapping("/random")
     public Profile getRandomProfile() {
         return profileRepository.getRandomProfile();
+    }
+
+    @GetMapping("/user")
+    public Profile getUserProfile(){
+        return profileService.getUserProfile();
     }
 
     // TODO add get profile by id because when you click again on the profile after opening a chat with them their profile will
---
+++ backend/src/main/java/projects/koko/tinder_backend/profiles/ProfileService.java
@@ -0,0 +1,19 @@
+package projects.koko.tinder_backend.profiles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import projects.koko.tinder_backend.utils.ProfileNotFoundException;
+
+import java.util.Optional;
+
+@Service
+public class ProfileService {
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    public Profile getUserProfile(){
+        return profileRepository.findById("user").orElseThrow(() -> new ProfileNotFoundException("User profile not found"));
+    }
+
+}
